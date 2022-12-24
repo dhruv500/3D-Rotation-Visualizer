022c78a469c5df82a76557bb96240ff0e47f1cac
dhruv <dhruv@tor.us>
2022-12-24 22:19:06 +0530

    Initial Commit

---
+++ 3D Rotation Visualizer/_D_Rotation_VisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  _D_Rotation_VisualizerApp.swift
+//  3D Rotation Visualizer
+//
+//  Created by Dhruv Jaiswal on 24/12/22.
+//
+
+import SwiftUI
+
+@main
+struct _D_Rotation_VisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
