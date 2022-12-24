31f2b86e07cb8e5516be744faa7f6da2a1efc33d
dhruv <dhruv@tor.us>
2022-12-24 23:04:56 +0530

    first commit

--- 3D Rotation Visualizer/ContentView.swift
+++ 3D Rotation Visualizer/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var x: CGFloat = 0
+    @State var y: CGFloat = 0
+    @State var z: CGFloat = 0
+    @State var degress: Double = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 0) {
+            VStack {
+                Text("3D Rotation Visualiser")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            .frame(height: 50)
+            VStack {
+                Spacer()
+                Rectangle()
+                    .fill(LinearGradient(gradient: .init(colors: [Color.orange, Color.pink]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                    .frame(width: 300, height: 200)
+                    .rotation3DEffect(.degrees(degress), axis: (x: x, y: y, z: z))
+                Spacer()
+            }
+            .padding(.top, 30)
+            VStack(alignment: .center) {
+                Text("degress \(degress)")
+                Slider(value: $degress, in: 0 ... 360)
+                HStack(alignment: .center) {
+                    Text("X \(x)")
+                    Stepper("", value: $x, in: 0 ... 1)
+                }.padding()
+                HStack(alignment: .center) {
+                    Text("Y \(y)")
+                    Stepper("", value: $y, in: 0 ... 1)
+                }.padding()
+                HStack(alignment: .center) {
+                    Text("Z \(z)")
+                    Stepper("", value: $z, in: 0 ... 1)
+                }.padding()
+            }
         }
-        .padding()
     }
 }
 
